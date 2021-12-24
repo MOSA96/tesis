7fddfcbc21131a432312d6dd67fb51d9a79c1e9b
mosa96 <caracol696@ciencias.unam.mx>
2021-12-24 13:15:26 -0600

    second version

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+#Define your function here
+def test(x):
+    #if x > 1.0:
+    #    return x**2
+   # elif x== 1:
+       # return 0
+    return x**2
+
+def function(individual):
+    x = individual[0]
+    f = test(x)
+    return f,
+
+def ind(low=-20, high=20):
+    return [np.random.uniform(low=low, high=high)]
